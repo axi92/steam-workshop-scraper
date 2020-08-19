5270c53d29d79bc22f4bec115eb47b5c0df3ade4
axi92 <phips_92@yahoo.de>
2020-08-19 13:14:39 +0200

    rework to class, TODO: figure out timezone conversion for PT to local

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
 const scrapeIt = require("scrape-it");
 const moment = require('moment');
 
-// Promise interface
-scrapeIt("https://steamcommunity.com/sharedfiles/filedetails/?id=670764308", {
-  updateDate: "div.detailsStatRight:nth-last-of-type(1)",
-}).then(({
-  data,
-  response
-}) => {
-  console.log(`Status Code: ${response.statusCode}`)
-  if (response.statusCode == 200) {
-    console.log(data.updateDate);
-    let date = data.updateDate
-    // console.log(moment(date, [DD MMM ]));
+
+
+
+class SteamWorkshopScraper {
+  constructor(lang, timezoneOffset) {
+    moment.locale(lang);
+    this.scrapeItHeader = {
+      "Accept-Language": lang
+      // TODO: find fix for timezoneOffset automation
+      // Cookie: "timezoneOffset=7200,0"
+    };
+    this.workShopUrl = 'https://steamcommunity.com/sharedfiles/filedetails/?id=';
+    this.scrapPathLastUpdateTime = 'div.detailsStatRight:nth-last-of-type(1)'
   }
-})
 
+  GetLastUpdateTimestamp(id) {
+    console.log(this.workShopUrl + id.toString());
+    this.Scrape(this.workShopUrl + id.toString(), this.scrapPathLastUpdateTime)
+  }
 
-// old mod
-// scrapeIt("https://steamcommunity.com/sharedfiles/filedetails/?id=1384657523", {
-//   updateDate: "div.detailsStatRight:nth-last-of-type(1)",
-// }).then(({
-//   data,
-//   response
-// }) => {
-//   console.log(`Status Code: ${response.statusCode}`)
-//   if (response.statusCode == 200) {
-//     console.log(data.updateDate);
-//     // console.log(moment(data.updateDate));
-//   }
-// })
+  Scrape(url, parse){
+    scrapeIt({
+      url: url,
+      headers: this.scrapeItHeader
+    }, {
+      updateDate: parse
+    }).then(({
+      data,
+      response
+    }) => {
+      // TODO: handle status code and error messages
+      // console.log(`Status Code: ${response.statusCode}`)
+      if (response.statusCode == 200) {
+        // console.log(response);
+        console.log(data.updateDate);
+        let date = data.updateDate
+        this.ParseSteamTime(date);
+      }
+    });
+  }
 
+  ParseSteamTime(string) {
+    if (string.match('[0-9]{4}')) {
+      return moment(string, 'DD MMM YYYY hh:mm').format("dddd, MMMM Do YYYY, HH:mm");
+    } else {
+      return moment(string, 'DD. MMM hh:mm').format("dddd, MMMM Do YYYY, HH:mm");
+    }
+  }
+}
 
-// without update
-// scrapeIt("https://steamcommunity.com/sharedfiles/filedetails/?id=589205263", {
-//   updateDate: "div.detailsStatRight:nth-last-of-type(1)",
-// }).then(({
-//   data,
-//   response
-// }) => {
-//   console.log(`Status Code: ${response.statusCode}`)
-//   if (response.statusCode == 200) {
-//     console.log(data.updateDate);
-//     // console.log(moment(data.updateDate));
-//   }
-// })
+var sws = new SteamWorkshopScraper('de');
+
+sws.GetLastUpdateTimestamp(670764308); //up to date 18. Aug. um 16:18 Uhr
+// sws.GetLastUpdateTimestamp(1384657523); //old mod
+// sws.GetLastUpdateTimestamp(589205263); //never updated
--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 const moment = require('moment');
-const tz = require('moment-timezone');
+const momentTZ = require('moment-timezone');
 
-// console.log(moment('18 Aug @ 7:18am PDT', 'DD MMM @ h:mm').add(9, 'hours'));
-console.log(tz('18 Aug @ 7:18am', 'PDT'));
+
+
+// 18 Aug @ 7:18am
+// 22 Apr, 2016 @ 12:43pm
+
+// US/Pacific
+// America/Los_Angeles
+// console.log(momentTZ.tz.guess());
+console.log(momentTZ.tz('18 Aug @ 7:18am', 'US/Pacific')); // , 'DD MMM H:mma'
+// moment.locale('de');
+// console.log(moment('18. Aug. um 16:18 Uhr', 'DD. MMM. hh:mm').format("dddd, MMMM Do YYYY, HH:mm"));
+
+
+// console.log(moment('18. Mai 2018 um 23:39 Uhr', 'DD. MMM. YYYY hh:mm').format("dddd, MMMM Do YYYY, HH:mm"));
 
 // +9h
